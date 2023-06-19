e15ecea1bf3cbfa6a01ea8bd613d0f4ecf649c3d
GHOST <136559366+Surjo178@users.noreply.github.com>
2023-06-19 21:30:02 +0600

    Create SF.py

---
+++ SF.py
@@ -0,0 +1,26 @@
+[+]=============================
+[+]               Author:Saiful Islam              =         
+[+]               Github:Surjo178                   =             
+[+]               Working: Data/Wifi               =   
+[+]               Tool Type:Free                      =            
+[+]               Version:0.3                            =            
+[+]=============================
+a=random_crack=pass "fast"
+b=random_system= ("print")
+c=random_facebook= "id"
+d=random_crack "print"_user
+e=random_id=get "cookies"
+f={print} "print"+code+game_random="result"
+g=key_approve "request"_print=code+random="string"
+h=working_bit=("arm64")
+i=random_crack ("vip")+user+game=get "result"
+j=random_crack ("facebook")=free "tool"
+k=random_crack (print) "vip" "ok user"
+l=cookies_"special" ("ok ids") {print}=user+id+code=get "result"
+m=random "id"=get "ok ids"
+n=("new_("auto cookies")
+o=get ("id")_crack "fast" (print)_get= "cookies"
+p=working_device="Symphoney"
+q=random_"crack"_"fast"=get ("cookies")
+r=crack_id "fast"=("11digit number")
+s=working_network=("data") {"wifi"}
